116834a69a1be3bfb383a2498658d4ad01cab4e9
AlexandreRJF <146349116+AlexandreRJF@users.noreply.github.com>
2024-03-13 18:04:40 +0100

    les persos peuvent bouger (mais faut spam la touche)

--- stick_smash/Assets/Scripts/Battle/player/player_manager.cs
+++ stick_smash/Assets/Scripts/Battle/player/player_manager.cs
@@ -7,24 +7,40 @@ public class player_manager : MonoBehaviour
 
     public Rigidbody2D rb;
     public float move_direction;
+    int speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 200;
+        move_direction = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("up") || Input.GetKeyDown("down") || Input.GetKeyDown("left") || Input.GetKeyDown("right")) {
+        /*if (Input.GetKeyDown("up") || Input.GetKeyDown("down") || Input.GetKeyDown("left") || Input.GetKeyDown("right")) {
             
             rb.velocity = new Vector2(move_direction * 450 * Time.fixedDeltaTime, rb.velocity.y);
-        }
+        }*/
 
-        if (Input.GetKeyDown("w") || Input.GetKeyDown("s") || Input.GetKeyDown("a") || Input.GetKeyDown("d")) {
+        /*if (Input.GetKeyDown("w") || Input.GetKeyDown("s") || Input.GetKeyDown("a") || Input.GetKeyDown("d")) {
 
             rb.velocity = new Vector2(move_direction * 450 * Time.fixedDeltaTime, rb.velocity.x);
+        }*/
+
+        // Get Player Movement Input
+        move_direction = (Input.GetAxisRaw("Horizontal")); 
+
+        if (Input.GetKeyDown("right") || Input.GetKeyDown("d")) {
+            
+            rb.velocity = new Vector2(move_direction * speed * Time.fixedDeltaTime, rb.velocity.y);
         }
+
+        if (Input.GetKeyDown("left") || Input.GetKeyDown("a")) {
+
+            rb.velocity = new Vector2(move_direction * speed * Time.fixedDeltaTime, rb.velocity.x);
+        }
+
     }
 }
