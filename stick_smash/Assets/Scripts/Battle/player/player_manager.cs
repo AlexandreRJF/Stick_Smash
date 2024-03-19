2acc50d10b95c0510c1d5f3ff24d479a5c5d8acb
AlexandreRJF <146349116+AlexandreRJF@users.noreply.github.com>
2024-03-19 17:00:37 +0100

    saut fait

--- stick_smash/Assets/Scripts/Battle/player/player_manager.cs
+++ stick_smash/Assets/Scripts/Battle/player/player_manager.cs
@@ -7,40 +7,44 @@ public class player_manager : MonoBehaviour
 
     public Rigidbody2D rb;
     public float move_direction;
-    int speed;
+    int vitesse;
+    int force_saut;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 200;
+        vitesse = 200;
+        force_saut = 8;
         move_direction = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKeyDown("up") || Input.GetKeyDown("down") || Input.GetKeyDown("left") || Input.GetKeyDown("right")) {
-            
-            rb.velocity = new Vector2(move_direction * 450 * Time.fixedDeltaTime, rb.velocity.y);
-        }*/
-
-        /*if (Input.GetKeyDown("w") || Input.GetKeyDown("s") || Input.GetKeyDown("a") || Input.GetKeyDown("d")) {
-
-            rb.velocity = new Vector2(move_direction * 450 * Time.fixedDeltaTime, rb.velocity.x);
-        }*/
 
         // Get Player Movement Input
         move_direction = (Input.GetAxisRaw("Horizontal")); 
 
         if (Input.GetKeyDown("right") || Input.GetKeyDown("d")) {
             
-            rb.velocity = new Vector2(move_direction * speed * Time.fixedDeltaTime, rb.velocity.y);
+            rb.velocity = new Vector2(move_direction * vitesse * Time.fixedDeltaTime, rb.velocity.y);
         }
 
         if (Input.GetKeyDown("left") || Input.GetKeyDown("a")) {
 
-            rb.velocity = new Vector2(move_direction * speed * Time.fixedDeltaTime, rb.velocity.x);
+            rb.velocity = new Vector2(move_direction * vitesse * Time.fixedDeltaTime, rb.velocity.x);
         }
 
+        if (Input.GetKeyDown("space") || Input.GetKeyDown("[0]")) {
+
+            jump();
+        }
+    }
+
+
+    // Permet de sauter
+    void jump() {
+
+        rb.velocity = Vector2.up * force_saut;
     }
 }
