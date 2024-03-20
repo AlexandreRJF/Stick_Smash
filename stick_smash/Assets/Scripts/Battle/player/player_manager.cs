1bce400d0d67712f2f571191726c4186976c1f32
AlexandreRJF <146349116+AlexandreRJF@users.noreply.github.com>
2024-03-20 16:09:24 +0100

    coder la vérif que le joueur est au sol (pour empêcher le jump spam)

--- stick_smash/Assets/Scripts/Battle/player/player_manager.cs
+++ stick_smash/Assets/Scripts/Battle/player/player_manager.cs
@@ -9,6 +9,7 @@ public class player_manager : MonoBehaviour
     public float move_direction;
     int vitesse;
     int force_saut;
+    bool is_on_ground;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class player_manager : MonoBehaviour
         vitesse = 200;
         force_saut = 8;
         move_direction = 1;
+        is_on_ground = false;
     }
 
     // Update is called once per frame
@@ -23,7 +25,7 @@ public class player_manager : MonoBehaviour
     {
 
         // Get Player Movement Input
-        move_direction = (Input.GetAxisRaw("Horizontal")); 
+        move_direction = (Input.GetAxisRaw("Horizontal"));
 
         if (Input.GetKeyDown("right") || Input.GetKeyDown("d")) {
             
@@ -37,7 +39,21 @@ public class player_manager : MonoBehaviour
 
         if (Input.GetKeyDown("space") || Input.GetKeyDown("[0]")) {
 
+            verif_jump();
+        }
+    }
+
+
+    // Permet de vérifier que le joueur peut sauter
+    void verif_jump() {
+
+        if (is_on_ground == true) {
+            
             jump();
+        }
+
+        else {
+            Debug.Log("is_on_ground est false");
         }
     }
 
@@ -46,5 +62,19 @@ public class player_manager : MonoBehaviour
     void jump() {
 
         rb.velocity = Vector2.up * force_saut;
+    }
+
+
+    // Permet de détecter quand le perso arrive au sol
+    void OnCollisionEnter2D(Collision2D hit_box) {
+        
+        is_on_ground = true;
+    }
+
+
+    // Permet de détecter quand le perso quitte le sol
+    void OnCollisionExit2D(Collision2D hit_box) {
+        
+        is_on_ground = false;
     }
 }
