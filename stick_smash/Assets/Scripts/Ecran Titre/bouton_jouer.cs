953db5507a4cacdaa7247d67b4309c065ac32638
AlexandreRJF <146349116+AlexandreRJF@users.noreply.github.com>
2024-05-23 13:11:58 +0200

    ajout du floue / meilleur couleurs persos / contour persos / bug fix

--- stick_smash/Assets/Scripts/Ecran Titre/bouton_jouer.cs
+++ stick_smash/Assets/Scripts/Ecran Titre/bouton_jouer.cs
@@ -6,9 +6,16 @@ using UnityEngine.SceneManagement;
 public class bouton_jouer : MonoBehaviour
 {
     
-    // Permet de passer sur la scène battle field
-    public void ouvrir_battle_field() {
+    // Permet de passer sur la scène battle field 1
+    public void ouvrir_battle_field_1() {
 
-        SceneManager.LoadScene("Battle");
+        SceneManager.LoadScene("Battle 1");
+    }
+
+
+    // Permet de passer sur la scène battle field 2
+    public void ouvrir_battle_field_2() {
+
+        SceneManager.LoadScene("Battle 2");
     }
 }
